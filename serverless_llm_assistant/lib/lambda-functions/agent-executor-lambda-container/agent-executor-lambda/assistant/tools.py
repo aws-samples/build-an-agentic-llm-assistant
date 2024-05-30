004dcf98193e0fec5ac36c1b15d7f6aa607e0561
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2024-05-30 09:39:08 +0000

    Refactor to use langchain_aws.BedrockLLM

--- serverless_llm_assistant/lib/lambda-functions/agent-executor-lambda-container/agent-executor-lambda/assistant/tools.py
+++ serverless_llm_assistant/lib/lambda-functions/agent-executor-lambda-container/agent-executor-lambda/assistant/tools.py
@@ -1,7 +1,7 @@
 # This module will be edited in Lab 03 to add the agent tools.
 import boto3
 from langchain.agents import Tool
-from langchain.llms.bedrock import Bedrock
+from langchain_aws import BedrockLLM
 from langchain_community.tools import DuckDuckGoSearchRun
 from .calculator import CustomCalculatorTool
 from .config import AgenticAssistantConfig
@@ -9,8 +9,12 @@ from .config import AgenticAssistantConfig
 config = AgenticAssistantConfig()
 bedrock_runtime = boto3.client("bedrock-runtime", region_name=config.bedrock_region)
 
-claude_llm = Bedrock(
+claude_llm = BedrockLLM(
     model_id=config.llm_model_id,
     client=bedrock_runtime,
-    model_kwargs={"max_tokens_to_sample": 500, "temperature": 0.0},
+    model_kwargs={
+        "max_tokens": 1000,
+        "temperature": 0.0,
+        "top_p": 0.99
+    },
 )
