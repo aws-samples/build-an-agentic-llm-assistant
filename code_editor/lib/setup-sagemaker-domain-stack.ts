099ccd2fdd4883824dfa26dcadfaef4602da9671
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2024-08-02 10:51:27 +0000

    Add cdk stack to setup a code editor for workshop labs

---
+++ code_editor/lib/setup-sagemaker-domain-stack.ts
@@ -0,0 +1,75 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as sagemaker from 'aws-cdk-lib/aws-sagemaker';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class SagemakerDomainWithCodeEditorStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // import existing default VPC
+    const existingDefaultVpc = ec2.Vpc.fromLookup(this, 'ExistingDefaultVPC', {isDefault: true});
+
+     // Create a SageMaker execution role
+    const sagemakerExecutionRole = new iam.Role(this, 'SageMakerExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('sagemaker.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSageMakerFullAccess'),
+      ],
+    });
+
+    // Create a SageMaker Domain
+    const domain = new sagemaker.CfnDomain(this, 'SageMakerDomain', {
+      vpcId: existingDefaultVpc.vpcId,
+      authMode: 'IAM',
+      domainName: 'agentic-assistant-domain',
+      // Use private subnets
+      subnetIds: existingDefaultVpc.publicSubnets.map(subnet => subnet.subnetId),
+      defaultUserSettings: {
+        executionRole: sagemakerExecutionRole.roleArn
+      }
+    });
+
+    const userProfile = new sagemaker.CfnUserProfile(this, 'DefaultUserProfile', {
+      domainId: domain.attrDomainId,
+      userProfileName: "default-profile"
+    })
+
+    const space = new sagemaker.CfnSpace(this, 'EditorSpace', {
+      spaceName: 'agentic-assistant-code-editor',
+      domainId: domain.attrDomainId,
+      ownershipSettings: {
+        ownerUserProfileName: userProfile.userProfileName
+      },
+      spaceSharingSettings: {
+        sharingType: 'Private'
+      },
+      spaceSettings: {
+        appType: 'CodeEditor',
+        spaceStorageSettings: {
+          ebsStorageSettings: {
+            ebsVolumeSizeInGb: 30
+          }
+        },
+        codeEditorAppSettings: {
+          defaultResourceSpec: {
+            instanceType: 'ml.m5.large'
+          }
+        }
+      }
+    })
+
+    // Resources a provisioned in Parallel by CloudFormation.
+    // we need this explicit dependency to avoid cases where
+    // the space creation is attempted before the user profile is ready.
+    space.addDependency(userProfile);
+
+    new cdk.CfnOutput(this, 'SageMakerDomainOutput', {
+      value: domain.attrDomainArn,
+      description: 'The ARN of the SageMaker Domain',
+      // exportName: 'SageMakerDomainWithCodeEditorARN',
+    });
+
+  }
+}
---
+++ code_editor/test/setup-sagemaker-domain.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SetupSagemakerDomain from '../lib/setup-sagemaker-domain-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/setup-sagemaker-domain-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SetupSagemakerDomain.SagemakerDomainWithCodeEditorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
